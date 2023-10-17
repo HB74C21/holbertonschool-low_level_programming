f0cb3165d3b0b30cc0122c1d960b5ed842c8cc6f
HB74C21 <6720@holbertonstudents.com>
2023-10-17 04:22:16 -0700

    Add new program

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Function that adds two integers and returns the result
+ *
+ * @num1: The first integers
+ * @num2: The second integers
+ *
+ * Return: The result of adds
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
