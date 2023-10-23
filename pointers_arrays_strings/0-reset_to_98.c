d74e1ca3bac978d6e834544813ddc8db41f8e7c2
HB74C21 <6720@holbertonstudents.com>
2023-10-23 01:24:54 -0700

    Add new program

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Function that takes a pointer to an int and update the value
+ *
+ * @n: This is integer
+*/
+void reset_to_98(int *n)
+{
+	int new_value = 98;
+
+	*n = new_value;
+}
