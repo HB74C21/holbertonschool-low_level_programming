98439402a75321a080913c3d59d769aabe551f1b
HB74C21 <6720@holbertonstudents.com>
2023-10-23 03:43:03 -0700

    Add new program

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - Function that prints a string in reverse
+ *
+ * @s: This is a string
+*/
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count])
+		count++;
+
+	while (s[count] >= 0)
+	{
+		_putchar(s[count]);
+		count--;
+	}
+
+	_putchar('\n');
+}
