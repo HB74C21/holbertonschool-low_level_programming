c1dbd0d6ee35e9c855a895ae6e2715d827970c51
HB74C21 <6720@holbertonstudents.com>
2023-10-25 01:14:22 -0700

    Add description for Return of _strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,6 +4,8 @@
  *		terminating null byte (\0), to the buffer pointed to by dest.
  * @dest: this is a character
  * @src: this is a string
+ *
+ * Return: The value the pointer to dest
 */
 char *_strcpy(char *dest, char *src)
 {
