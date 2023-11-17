5c57c5e3400ec8c505b1b9c6f63625b140553c86
HB74C21 <6720@holbertonstudents.com>
2023-11-17 11:29:04 -0800

    Add new prototype

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
-struct dog
+/**
+ * struct dog - Create a struct of a dog
+ *
+ * @name: This is a name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+*/
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
