7050565c1644f3125160bd57fde5a0755403a74c
HB74C21 <6720@holbertonstudents.com>
2023-11-19 08:32:29 -0800

    Add new program

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,13 +6,13 @@
  * @size: This is a size of array
  * @cmp: This is a pointer of function
  *
- * Return: value of int or  -1 if no element matches or -1 if size <= 0
+ * Return: value of int or -1 if no element matches or -1 if size <= 0
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 && array == NULL && cmp == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
