421973fc8fc3f0d844410575d26afd0ba501f743
HB74C21 <6720@holbertonstudents.com>
2023-11-21 06:32:31 -0800

    fix error

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -22,7 +22,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			num = va_arg(ap, int);
-			printf("%s%d", separator, num);
+
+			if (i < n - 1)
+				printf("%d%s", num, separator);
+
+			else
+				printf("%d", num);
 		}
 	}
 	printf("\n");
