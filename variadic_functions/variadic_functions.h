2d4180930133b79c1e566ed2f5da771bb3861104
HB74C21 <6720@holbertonstudents.com>
2023-11-22 14:08:52 -0800

    Add new prototype

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,26 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
+#include <stdarg.h>
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+*/
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list ap);
+} op_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void type_c(va_list ap);
+void type_i(va_list ap);
+void type_f(va_list ap);
+void type_s(va_list ap);
 
 #endif
