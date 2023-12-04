867a729fc3412ad5e5b5e4cc3ed94dcf187298b4
HB74C21 <6720@holbertonstudents.com>
2023-12-04 08:50:51 -0800

    Add new condition if b is NULL return (0)

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,9 @@ unsigned int binary_to_uint(const char *b)
 	int count = 0;
 	unsigned int result = 0;
 
+	if (b == NULL)
+		return (0);
+
 	while (b[lenght] != '\0')
 	{
 		if (b[lenght] != '0' && b[lenght] != '1')
