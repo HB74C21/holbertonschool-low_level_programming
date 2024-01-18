64857a247e716fce1d6fcd4312398cfd8d0053ea
HB74C21 <6720@holbertonstudents.com>
2024-01-18 11:45:07 +0000

    Add header file and 0-linear.c for function that searches for a value in a sorted array of integers using the Binary search algorithm

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+* linear_search - Function that searches for a value in an array of integers
+*               using the Linear shearch algotithm
+* @array: Thus us a pointer to the first element of the array to search in
+* @size: This is a number elements in array
+* @value: This is a value to search for
+*
+* Return: -1 if value is not present in array or if array is NULL
+*		otherwise Return value of index
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index_array;
+
+	for (index_array = 0; index_array < size; index_array++)
+	{
+		printf("Value checked array [%ld] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+		{
+			return (index);
+		}
+	}
+
+	return (-1);
+}
